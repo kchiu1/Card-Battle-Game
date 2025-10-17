9945ec66c1923140216cb61e139c955d8c3cda77
Kyle Chiu <152306707+kchiu1@users.noreply.github.com>
2025-10-17 16:33:56 -0400

    Simulated battle (doesn't work)

--- card-battle-game/Scripts/Cards/Card.gd
+++ card-battle-game/Scripts/Cards/Card.gd
@@ -3,22 +3,22 @@ class_name Card
 
 # Card properties
 @export var card_name: String = "Strike"
-@export var min: int = 0
-@export var max: int = 0
+@export var lower: int = 0
+@export var upper: int = 0
 @export var type: String = "Attack" # Can be "Attack", "Defense", "Magic", etc.
 @export var effects: Array = []  
 @export var attached_items: Array = [] # Items that can be used with this card
 
 #initialization
-func _init( _card_name: String = "Strike", _min: int = 0, _max: int = 0, _type: String = "Attack", _effects: Array = []):
+func _init( _card_name: String = "Strike", _lower: int = 0, _upper: int = 0, _type: String = "Attack", _effects: Array = []):
 	card_name = card_name
 
-	min = _min
-	max = _max
+	lower = _lower
+	upper = _upper
 	
 	type = _type
 	effects = _effects.duplicate()
 	attached_items = []
 	
 func roll() -> int:
-	return randi() % (max - min + 1) + min
+	return randi() % (upper - lower + 1) + lower
