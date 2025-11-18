5dd285a12e824aa9eb927e31ae2646de23e40bf9
Kevin Chen <kchenpc@gmail.com>
2025-11-18 15:29:51 -0500

    Added Card Data
    Can now dynamically adjust cards based on data from CSV

--- card-battle-game/Scenes/BoardScene/deck.gd
+++ card-battle-game/Scenes/BoardScene/deck.gd
@@ -2,19 +2,39 @@ extends Node2D
 
 const CARD_SCENE_PATH = "res://Common/Cards/TestCard.tscn"
 const CARD_DRAW_SPEED = 0.5
+const PLAYER_HAND_SIZE = 3
 
-var player_deck = ["Bot", "Bot", "Bot"]
+var player_deck = [1, 1, 1, 1, 2, 2, 2, 3, 4]
+var discard = []
+var card_database_reference
+var card_scene = preload(CARD_SCENE_PATH)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+    player_deck.shuffle()
+    var CardDatabase = preload("res://Scripts/Cards/CardDatabase.gd")
+    card_database_reference = CardDatabase.new()
+    card_database_reference.load_cards()
 
 
 func draw_card():
     
-    var card_scene = preload(CARD_SCENE_PATH)
+    while($"../PlayerHand".player_hand.size() < PLAYER_HAND_SIZE):
+        if(player_deck.is_empty()):
+            discard.shuffle()
+            player_deck.append_array(discard)
+            discard.clear()
+        
+        var card_drawn = player_deck[0]
+        discard.append(player_deck.pop_front())
+        
+        var new_card = card_scene.instantiate()
+        var card_image_path = str("res://Assets/" + card_database_reference.cards[card_drawn]["name"] + "Card.png")
+        new_card.get_node("CardImage").texture = load(card_image_path)
+        new_card.get_node("ClashValue").text = str(card_database_reference.cards[card_drawn]["min"]) + "-" + str(card_database_reference.cards[card_drawn]["max"])
+        new_card.get_node("Name").text = card_database_reference.cards[card_drawn]["name"]
+        $"../PlayerHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
+        
+        $"../CardManager".add_child(new_card)
+        new_card.name = "Card"
     
-    var new_card = card_scene.instantiate()
-    $"../CardManager".add_child(new_card)
-    new_card.name = "Card"
-    $"../PlayerHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
--- card-battle-game/Scripts/Cards/CardDatabase.gd
+++ card-battle-game/Scripts/Cards/CardDatabase.gd
@@ -4,35 +4,50 @@ class_name CardDatabase
 var cards: Dictionary = {}
 
 func _ready():
-	var file = FileAccess.open("res://data/CardList.csv", FileAccess.READ)
-	if file:
-		var header = file.get_line().strip_edges().split(",")
-		while not file.eof_reached():
-			var line = file.get_line().strip_edges()
-			if line == "":
-				continue
-			var data = line.split(",")
-			if data.size() < header.size():
-				continue
-			var card_data = {}
-			for i in range(header.size()):
-				card_data[header[i]] = data[i]
-			var id = int(card_data["id"])
-			cards[id] = card_data
-		file.close()
-	else:
-		print("Failed to open CardList.csv")
+    load_cards()
+
+func load_cards():
+    var file = FileAccess.open("res://data/CardList.csv", FileAccess.READ)
+    if not file:
+        print("Failed to open CardList.csv")
+        return
+        
+    var header = file.get_line().strip_edges().split(",")
+    while not file.eof_reached():
+        var line = file.get_line().strip_edges()
+        if line == "":
+            continue
+        var data = line.split(",")
+        if data.size() < header.size():
+            continue
+        var card_data = {}
+        for i in range(header.size()):
+            var key = header[i]
+            var value = data[i].strip_edges()
+            if value.begins_with("[") and value.ends_with("]"):
+                var inner = value.substr(1, value.length() - 2)
+                if inner == "":
+                    card_data[key] = []
+                else:
+                    card_data[key] = inner.split(";", false)
+            else:
+                card_data[key] = value
+                
+        var id = int(card_data["id"])
+        cards[id] = card_data
+    print(cards)
+    file.close()
 
 func get_card(card_id: int) -> Card:
-	if not cards.has(card_id):
-		print("Card not found: %d" % card_id)
-		return null
+    if not cards.has(card_id):
+        print("Card not found: %d" % card_id)
+        return null
 
-	var card = cards[card_id]
-	return Card.new(
-		card["name"],
-		int(card["value_min"]),
-		int(card["value_max"]),
-		card["type"],
-		card["effects"].split(";", false)
-	)
+    var card = cards[card_id]
+    return Card.new(
+        card["name"],
+        int(card["min"]),
+        int(card["max"]),
+        card["type"],
+        card["effects"]
+    )
