e988fed3801984378463411b8dfc6765b57a48e2
Kyle Chiu <152306707+kchiu1@users.noreply.github.com>
2026-02-24 14:36:32 -0500

    New weapon graphics again

--- card-battle-game/main/main_menu.gd
+++ card-battle-game/main/main_menu.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 
 func _on_quit_pressed():
-    get_tree().quit()
+	get_tree().quit()
 
 
 func _on_start_pressed() -> void:
-    get_tree().change_scene_to_file("res://Scenes/FightScene/FightScene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/FightScene/FightScene.tscn")
